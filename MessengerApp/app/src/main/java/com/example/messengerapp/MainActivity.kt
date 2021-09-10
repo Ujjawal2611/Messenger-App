1a6a52e2eae057436bb7947bf1e10374281ed303
Ujjawal Parasar <8228952880supaul@gmail.com>
2021-09-10 14:21:21 +0530

    Initial Commit

--- MessengerApp/app/src/main/java/com/example/messengerapp/MainActivity.kt
+++ MessengerApp/app/src/main/java/com/example/messengerapp/MainActivity.kt
@@ -5,7 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.example.messengerapp.adapters.ViewPagerAdapter
 import com.example.messengerapp.databinding.ActivityMainBinding
+import com.example.messengerapp.fragments.CallFragment
+import com.example.messengerapp.fragments.ChatFragment
+import com.google.android.material.tabs.TabLayoutMediator
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -13,12 +18,20 @@ import com.google.firebase.ktx.Firebase
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
     private lateinit var auth:FirebaseAuth
+    private val fragList=ArrayList<Fragment>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbarMainMenu)
         auth=Firebase.auth
+        fragList.add(CallFragment())
+        fragList.add(ChatFragment())
+        binding.ViewPagerMain.adapter=ViewPagerAdapter(fragList,this)
+        val arr= arrayOf("Chat","Call","Status")
+        TabLayoutMediator(binding.tabLayoutMain, binding.ViewPagerMain) { tab, position ->
+            tab.text = arr[position]
+        }.attach()
 
     }
 
